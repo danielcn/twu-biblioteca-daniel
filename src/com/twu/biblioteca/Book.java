6311293799d2ffa68dde28b7a497f0b6decf4f67
Daniel Nascimento <dnascimen@MacBook-Pro-de-Daniel.local>
2018-06-21 06:46:54 -0300

    Adding method to return a book string list

---
+++ src/com/twu/biblioteca/Book.java
@@ -0,0 +1,11 @@
+package com.twu.biblioteca;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Book {
+
+    public List<String> getBookList() {
+        return Arrays.asList("book 1","book 2", "book 3");
+    }
+}
