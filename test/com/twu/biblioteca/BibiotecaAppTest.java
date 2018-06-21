dcb148174095a61629e4182d51236e761a38c8b2
Daniel Nascimento <dnascimen@MacBook-Pro-de-Daniel.local>
2018-06-21 19:35:52 -0300

    Removing unused imports

--- test/com/twu/biblioteca/BibiotecaAppTest.java
+++ test/com/twu/biblioteca/BibiotecaAppTest.java
@@ -1,12 +1,7 @@
 package com.twu.biblioteca;
 
-
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
-import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
