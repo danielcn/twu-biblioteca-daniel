81f5e68b0db157d6b6bf01476ddf1f253d46e86d
Daniel Nascimento <dnascimen@MacBook-Pro-de-Daniel.local>
2018-06-21 06:23:39 -0300

    Adding BookTest class

---
+++ test/com/twu/biblioteca/BookTest.java
@@ -0,0 +1,13 @@
+package com.twu.biblioteca;
+
+import org.junit.Test;
+
+public class BookTest {
+
+    private Book book;
+
+    @Test
+    public void testBookList(){
+        book =  new Book();
+    }
+}
