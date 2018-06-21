56b23b7441a5bd16ef2496e11755b70375632230
Daniel Nascimento <dnascimen@MacBook-Pro-de-Daniel.local>
2018-06-21 06:45:30 -0300

    Implementing book test list

--- test/com/twu/biblioteca/BookTest.java
+++ test/com/twu/biblioteca/BookTest.java
@@ -2,6 +2,11 @@ package com.twu.biblioteca;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
 public class BookTest {
 
     private Book book;
@@ -9,5 +14,7 @@ public class BookTest {
     @Test
     public void testBookList(){
         book =  new Book();
+        List<String> expepected = Arrays.asList("book 1","book 2", "book 3");
+        assertEquals(expepected, book.getBookList());
     }
 }
