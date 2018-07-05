6459c48cbe0b5748fa36a70d28edf529e5a89f41
Daniel Nascimento <dnascimen@MacBook-Pro-de-Daniel.local>
2018-07-05 14:50:01 -0300

    Refactoring adding @Before to set up object

--- test/com/twu/biblioteca/BibiotecaAppTest.java
+++ test/com/twu/biblioteca/BibiotecaAppTest.java
@@ -1,29 +1,31 @@
 package com.twu.biblioteca;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class BibiotecaAppTest {
 
     private BibliotecaApp bibliotecaApp;
 
+    @Before
+    public void setUp(){
+        bibliotecaApp = new BibliotecaApp();
+    }
+
     @Test
     public void testWelcomeMessage() {
-        bibliotecaApp = new BibliotecaApp();
         String welcome =  bibliotecaApp.welcomeMessage();
         assertEquals("Welcome Message", welcome);
     }
 
     @Test
     public void testGetBookList(){
-        bibliotecaApp = new BibliotecaApp();
         List<Book> actualBookList =  bibliotecaApp.getBookList();
         List<Book> expectedBookList = Arrays.asList(new Book("Harry Potter"), new Book("More and better"), new Book("The Alice's Life"));
         assertArrayEquals(expectedBookList.toArray(), actualBookList.toArray());
@@ -31,28 +33,20 @@ public class BibiotecaAppTest {
 
     @Test
     public void testCheckOutBook(){
-        bibliotecaApp = new BibliotecaApp();
-
         Book book = new Book();
         book.setBookName("Harry Potter"); //it works if is equal or greater than second element
-
         assertTrue(bibliotecaApp.checkOutBook(book));
     }
 
     @Test
     public void testReturnBook(){
-        bibliotecaApp = new BibliotecaApp();
-
         Book bookExpected = new Book("Harry Potter");
         Book bookActual = bibliotecaApp.returnBook(bookExpected);
-
         assertEquals(bookExpected, bookActual);
     }
 
     @Test
     public void testReturnBookSuccessful(){
-        bibliotecaApp = new BibliotecaApp();
-
         Book bookExpected = new Book("Harry Potter");
         Book bookActual = bibliotecaApp.returnBook(bookExpected);
         if(bookActual != null) {
@@ -63,8 +57,6 @@ public class BibiotecaAppTest {
 
     @Test
     public void testReturnBookUnsuccessful(){
-        bibliotecaApp = new BibliotecaApp();
-
         Book bookExpected = new Book("Harry Potter");
         Book bookActual = bibliotecaApp.returnBook(bookExpected);
         if(bookActual != null) {
