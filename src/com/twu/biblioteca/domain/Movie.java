5f1462d5cf3471123b5b71ee4cf022f3e0c3df34
Daniel Nascimento <dnascimen@MacBook-Pro-de-Daniel.local>
2018-07-15 19:49:20 -0300

    Add Movie class

---
+++ src/com/twu/biblioteca/domain/Movie.java
@@ -0,0 +1,56 @@
+package com.twu.biblioteca.domain;
+
+import java.util.Objects;
+
+public class Movie {
+
+    private String name;
+    private String year;
+    private String directior;
+    private int rating;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getDirectior() {
+        return directior;
+    }
+
+    public void setDirectior(String directior) {
+        this.directior = directior;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Movie movie = (Movie) o;
+        return Objects.equals(name, movie.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
