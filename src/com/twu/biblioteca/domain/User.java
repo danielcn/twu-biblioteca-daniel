1618eb68c758812107c660f03ef93b759c78db42
Daniel Nascimento <dnascimen@MacBook-Pro-de-Daniel.local>
2018-07-16 07:36:53 -0300

    Adding new attributes to user

--- src/com/twu/biblioteca/domain/User.java
+++ src/com/twu/biblioteca/domain/User.java
@@ -4,12 +4,57 @@ import java.util.Objects;
 
 public class User {
 
+    public String name;
+    public String email;
+    public String address;
+    public String phoneNumber;
     private String libraryNumber; // xxx-xxxx
     private int password;
 
     public User(String libraryNumber, int password) {
         this.libraryNumber = libraryNumber;
         this.password = password;
+    }
+
+    public User(String name, String email, String addres, String phoneNumber, String libraryNumber, int password) {
+        this.name = name;
+        this.email = email;
+        this.address = addres;
+        this.phoneNumber = phoneNumber;
+        this.libraryNumber = libraryNumber;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
 
     public int getPassword() {
