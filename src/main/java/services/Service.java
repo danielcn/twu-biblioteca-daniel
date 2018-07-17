793efbdd1479aced734fd048618d5b3de11bd0ad
Daniel Nascimento <dnascimen@MacBook-Pro-de-Daniel.local>
2018-07-17 18:12:49 -0300

    Adding Service

---
+++ src/main/java/services/Service.java
@@ -0,0 +1,6 @@
+package services;
+
+public interface Service<T> {
+
+    void add(T item);
+}
---
+++ src/main/java/services/impl/BookService.java
@@ -0,0 +1,10 @@
+package services.impl;
+
+import domain.Book;
+import services.Service;
+
+public class BookService implements Service<Book> {
+    @Override
+    public void add(Book book){
+    }
+}
